21c1f75461fc117bf2a1ad6d1b650e267e31bfcd
Muhammad Qasim Ali <qasimm6453@gmail.com>
2024-11-22 22:56:25 +0500

    Added Fully Responsive Nvbar and Footer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     $('#my-navbar').load('/assets/pages/navbar.html');
+    $('#my-footer').load('/assets/pages/footer.html');
 });
+
+
+// Used in Footer Icon for going to Top
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
